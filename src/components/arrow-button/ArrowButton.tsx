4ee6cffc3f8554dc5ac419d0ad280e4051f68e6a
sogdian <sogdian4815@gmail.com>
2024-06-03 18:39:49 +0200

    дебаг ArrowButton

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,18 +1,31 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
+export type ArrowButtonProps = {
+	onClick: OnClick;
+	state: boolean;
+};
+
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export const ArrowButton = (props: ArrowButtonProps) => {
+	const { onClick, state } = props;
+
+	const arrowButtonStyle = clsx(styles.container, {
+		[styles.container_open]: state,
+	});
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+			onClick={onClick}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
+			className={arrowButtonStyle}>
 			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
 		</div>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,11 +2,18 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setOpen] = useState(false);
+
+	const handleClick = () => {
+		return setOpen(!isOpen);
+	};
+
 	return (
 		<>
-			<ArrowButton />
+			<ArrowButton onClick={handleClick} state={isOpen} />
 			<aside className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
